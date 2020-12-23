f84594fcb7a115cc60c2d9a38dbd2f07b0aed9f2
Ed Tan <edtan@users.noreply.github.com>
2020-12-23 18:32:30 -0500

    install vagrant

--- manjaro-kde.sh
+++ manjaro-kde.sh
@@ -19,9 +19,12 @@ sudo pacman -S \
   helm \
   keepassxc \
   kubectl \
+  nvm \
   python-graphviz \
   terraform \
   tree \
+  ttf-ms-fonts \
+  vagrant \
   xclip
 
 sudo systemctl enable docker
@@ -41,6 +44,14 @@ echo 'export PATH="$(go env GOPATH)/bin:$PATH"' >> ~/.bashrc
 # For spotify, we need to import the key, https://aur.archlinux.org/packages/spotify/
 # curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
 # spotify
+
+# nvm
+echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
+nvm install --lts
+nvm use --lts
+
+# serverless
+npm install -g serverless
 
 # install vim plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
